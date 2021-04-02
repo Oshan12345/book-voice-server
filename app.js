4b94717a785cc5ebcc605867f2a9e5f1cedf0ef9
Oshan12345 <bsagor99@gmail.com>
2021-04-02 15:42:21 +0600

    fixed some issues

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 const ObjectId = require("mongodb").ObjectId;
 const MongoClient = require("mongodb").MongoClient;
 
-const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASS}@cluster0.ocnvo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ocnvo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
