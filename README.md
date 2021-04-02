3be82cbba3959779d3d470b278a05e44aa3c447a
Oshan12345 <bsagor99@gmail.com>
2021-04-02 17:43:06 +0600

    deleted unnecessary stufs

---
+++ README.md
@@ -0,0 +1,20 @@
+# Getting Started with Create React App
+
+Live Site [Live site](https://github.com/facebook/create-react-app).
+
+## Project name - book-voice
+
+This is simple **MERN** fullstack project.  
+
+
+> ### API for
+>> Getting all products.  
+>> Getting product based on id  
+>> Record purchase order  
+>> Delete a product based on id  
+>> Update a product based on id  
+>> Get order details of each user based on email
+
+
+
+Clint Side code [server side](https://github.com/facebook/create-react-app).
--- app.js
+++ app.js
@@ -20,15 +20,11 @@ client.connect((err) => {
     .db(`${process.env.DB_NAME}`)
     .collection("booksCollection");
   const placedOrder = client.db("book_voice").collection("placedOrder");
-  // perform actions on the collection object
-
-  console.log("connected to db");
 
   //create data
   app.post("/insertProduct", (req, res) => {
     const product = req.body;
     productsCollection.insertOne(product).then((result) => {
-      console.log(result);
       res.send({
         message: "You have successfully inserted your product in database",
         isInserted: result.insertedCount > 0,
@@ -38,39 +34,31 @@ client.connect((err) => {
   //store information of placed order
   app.post("/placeOrder", (req, res) => {
     const orderDetails = req.body;
-    console.log(orderDetails);
     placedOrder.insertOne(orderDetails).then((result) => {
-      console.log(result);
       res.send({
         message: "You have successfully placed your order",
         isInserted: result.insertedCount > 0,
       });
     });
   });
+
   //Read database
   app.get("/getAllProducts", (req, res) => {
     productsCollection
       .find({})
       .toArray((error, documents) => res.send(documents));
   });
-  //read database based on id
 
+  //read database based on id
   app.get("/getProduct/:id", (req, res) => {
     const id = req.params.id;
-    console.log(id);
-    // productsCollection
-    //   .find({
-    //     _id: ObjectId(id),
-    //   })
-    //   .toArray((err, document) => {
-    //     res.send(document[0]);
-    //   });
     productsCollection
       .findOne({
         _id: ObjectId(id),
       })
       .then((result) => res.send(result));
   });
+
   // get all order placed by a single user
   app.get("/getOrderDetails/:email", (req, res) => {
     const email = req.params.email;
@@ -79,10 +67,12 @@ client.connect((err) => {
       res.send(document);
     });
   });
+
   //get all orders' data placed by users
   app.get("/getAllOrders", (req, res) => {
     placedOrder.find({}).toArray((error, documents) => res.send(documents));
   });
+
   //update product
   app.patch("/updateProduct/:id", (req, res) => {
     const id = req.params.id;
@@ -98,6 +88,7 @@ client.connect((err) => {
       )
       .then((result) => res.send({ message: "updated successfully" }));
   });
+
   //delete a product
   app.delete("/delete/:id", (req, res) => {
     const id = req.params.id;
@@ -107,6 +98,7 @@ client.connect((err) => {
       res.send({ message: "deleted successfully" });
     });
   });
+
   //search function
   // app.get("/search", function (req, res) {
   //   console.log(req.body.query);
@@ -117,10 +109,6 @@ client.connect((err) => {
   //     });
   // });
   //   //end of mongodb function
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
 });
 
 app.listen(process.env.PORT || port);
